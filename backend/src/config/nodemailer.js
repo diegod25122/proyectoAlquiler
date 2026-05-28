2ae4d39b795692bd1ffb57580ce74f52d3cec9d1
diegod25122 <diego.camacho@epn.edu.ec>
2026-05-28 15:14:38 -0500

    configuracion

--- backend/src/config/nodemailer.js
+++ backend/src/config/nodemailer.js
@@ -2,47 +2,25 @@ import nodemailer from "nodemailer"
 import dotenv from "dotenv"
 dotenv.config()
 
-
-
-
 const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 587,             
-  secure: false,        
-  auth: {
-    user: process.env.USER_MAILTRAP, 
-    pass: process.env.PASS_MAILTRAP, 
-  },
-  tls: {
-    rejectUnauthorized: false 
-  }
-});
-
-transporter.verify((error, success) => {
-  if (error) {
-    console.log("❌ Error de conexión con Gmail:", error);
-  } else {
-    console.log("✅ Servidor listo para enviar correos desde Gmail");
-  }
-});
-/**
- * Función genérica para enviar correos
- * @param {string} to - Email del destinatario
- * @param {string} subject - Asunto del correo
- * @param {string} html - Contenido HTML del correo
- */
+    host: process.env.HOST_MAILTRAP,
+    port: Number(process.env.PORT_MAILTRAP),
+    secure: false,
+    auth: {
+        user: process.env.USER_MAILTRAP,
+        pass: process.env.PASS_MAILTRAP,
+    }
+})
 
 const sendMail = async (to, subject, html) => {
-
     try {
         const info = await transporter.sendMail({
-            from: '"POLI - RENT" <admin@polirent.com>',
+            from: '"POLI - RENT" <jairomaigua81@gmail.com>',
             to,
             subject,
             html,
         })
         console.log("✅ Email enviado:", info.messageId)
-
     } catch (error) {
         console.error("❌ Error enviando email:", error.message)
     }
