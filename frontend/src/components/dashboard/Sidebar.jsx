049218b93703796956622e554a0c9ed5128edbaa
Maigua-Jairo <jairo.maigua@epn.edu.ec>
2026-06-25 07:27:16 -0500

    verificacion de rutas

--- frontend/src/components/dashboard/Sidebar.jsx
+++ frontend/src/components/dashboard/Sidebar.jsx
@@ -17,7 +17,7 @@ export const Sidebar = () => {
     const navLinks = [
         { to: '/dashboard', label: 'Dashboard', icon: '🏠' },
         { to: '/dashboard/list', label: 'Mis Alquileres', icon: '🛒' },
-        { to: '/dashboard/registrar-herramienta', label: 'Registrar Herramienta', icon: '🛠️' },
+        { to: '/dashboard/registrarherramienta', label: 'Registrar Herramienta', icon: '🛠️' },
         { to: '/dashboard/profile', label: 'Perfil', icon: '👤' }
 
     ]
