7844dae8d728c9268c7c96692d54d23d8e8104c8
Maigua-Jairo <jairo.maigua@epn.edu.ec>
2026-07-09 08:02:42 -0500

    Correccion en la importacion de librerias

--- backend/src/routers/reserva_routers.js
+++ backend/src/routers/reserva_routers.js
@@ -1,8 +1,8 @@
-import {Router} from "express"; 
-import {registrarReserva} from "../controllers/reserva_controller.js";
-import {verificarToken} from "../middlewares/JWT.js";
+import {Router} from "express" 
+import {registrarReserva} from "../controllers/reserva_controller.js"
+import {verificarTokenJWT} from "../middlewares/JWT.js"
 const router = Router();
 
-router.post("/registrarReserva",verificarToken,registrarReserva);
+router.post("/registrarReserva",verificarTokenJWT,registrarReserva);
 
 export default router;
