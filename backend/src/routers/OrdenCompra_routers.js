8e2107be2bbf8162df682097f2719e4e9bb53b42
Diego <diego@example.com>
2026-07-22 14:39:26 -0500

    correccion

--- backend/src/routers/OrdenCompra_routers.js
+++ backend/src/routers/OrdenCompra_routers.js
@@ -41,7 +41,7 @@ const router = Router()
  *                 items:
  *                   type: object
  *                   properties:
- *                     producto:
+ *                     productoId:
  *                       type: string
  *                       example: "64f1a2b3c4d5e6f7a8b9c0d1"
  *                     cantidad:
